be0359558c0263417533e07846f5681e56a0a9a4
attackondemand <michael.e.silvestre@gmail.com>
2019-12-05 00:30:39 -0500

    Updates

---
+++ precache-manifest.3c4a9534f0674f621a712c52de13ade7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3657c483167c47a622ccf0322d402a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38ab5214988fbe11388a",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "ed765bd4f153ec646442",
+    "url": "/static/css/main.da06e67b.chunk.css"
+  },
+  {
+    "revision": "38ab5214988fbe11388a",
+    "url": "/static/js/2.10f2fbe6.chunk.js"
+  },
+  {
+    "revision": "ed765bd4f153ec646442",
+    "url": "/static/js/main.29869766.chunk.js"
+  },
+  {
+    "revision": "c04912aef301fe63a703",
+    "url": "/static/js/runtime-main.ea0d3fb1.js"
+  },
+  {
+    "revision": "c2ccf976a76213a0231bf9cb03d314c6",
+    "url": "/static/media/Igor-Dyatlov.c2ccf976.jpg"
+  },
+  {
+    "revision": "911edbf54796e8540a88e0fa05991d6a",
+    "url": "/static/media/aleksander-kolevatov.911edbf5.jpg"
+  },
+  {
+    "revision": "68146cf62233fdce5f24441df946bedf",
+    "url": "/static/media/background-mountains.68146cf6.jpg"
+  },
+  {
+    "revision": "4c4cff369d6c9b6ae95fd6161cc4adab",
+    "url": "/static/media/lyudmila-dubinina.4c4cff36.jpg"
+  },
+  {
+    "revision": "b5cf7076a27690f88ac10a621d687c0e",
+    "url": "/static/media/nikolay-brignolle.b5cf7076.jpg"
+  },
+  {
+    "revision": "6650a2202c398334eaff60e8c720dc04",
+    "url": "/static/media/rustem-slobodin.6650a220.jpg"
+  },
+  {
+    "revision": "e30d3fc86a6db4b4f215cf982f150dda",
+    "url": "/static/media/semyon-zolotaryov.e30d3fc8.jpg"
+  },
+  {
+    "revision": "a6a39bcc9dd2ba79bbc263abb281e647",
+    "url": "/static/media/star-wars-logo.a6a39bcc.png"
+  },
+  {
+    "revision": "9cf22855852deb78b1d915086128a1dc",
+    "url": "/static/media/yuri-doroshenko.9cf22855.jpg"
+  },
+  {
+    "revision": "e17921712f0f6bde94174d15aa80318c",
+    "url": "/static/media/yuri-krivonischenko.e1792171.jpg"
+  },
+  {
+    "revision": "fb2f3d5e259d0f4ffcb8855ab3c96591",
+    "url": "/static/media/yuri-yudin.fb2f3d5e.jpg"
+  },
+  {
+    "revision": "469e12a07cc633b0258c8aed261a21dd",
+    "url": "/static/media/zinaida-kolmogorova.469e12a0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/assign7/precache-manifest.151739377f52cdb55badc875ce82b68a.js"
+  "/precache-manifest.3c4a9534f0674f621a712c52de13ade7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/assign7/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
